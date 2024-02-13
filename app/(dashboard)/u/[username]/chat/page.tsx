88d2d9020362d8d982de4553a2384817936f7ce1
kidjoker <superherojoke.gmail.com>
2024-02-12 21:59:11 -0600

    finish Stream Chat & Key setting

---
+++ app/(dashboard)/u/[username]/chat/page.tsx
@@ -0,0 +1,44 @@
+import { getSelf } from "@/lib/auth-service";
+import { getStreamByUserId } from "@/lib/stream-service";
+import { ToggleCard } from "./_components/toggle-card";
+import { Skeleton } from "@/components/ui/skeleton";
+
+const ChatPage = async () => {
+  const self = await getSelf();
+  const stream = await getStreamByUserId(self.id);
+
+  if (!stream) {
+    throw new Error("Stream not found");
+  }
+
+  return (
+    <div className="p-6">
+      <div className="mb-4">
+        <h1 className="text-2xl font-bold">Chat settings</h1>
+      </div>
+      <div className="space-y-4">
+        <ToggleCard
+          field="isChatEnabled"
+          label="Enable chat"
+          value={stream.isChatEnabled}
+        />
+        <ToggleCard
+          field="isChatDelayed"
+          label="Delay chat"
+          value={stream.isChatDelayed}
+        />
+        <ToggleCard
+          field="isChatFollowersOnly"
+          label="Must be following to chat"
+          value={stream.isChatFollowersOnly}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ChatPage;
+
+export const ToggleCardSkeleton = () => {
+  return <Skeleton className="rounded-xl p-10 w-full" />;
+};
